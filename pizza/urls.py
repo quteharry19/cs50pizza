809144b3dbca0f9c3f874f62c9465d77884c59a8
Harish <quteharry19@hotmail.com>
2019-08-26 10:47:14 +0530

    html pages update

--- pizza/urls.py
+++ pizza/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("orders.urls")),
-#    path("accounts/", include('django.contrib.auth.urls')),
+    path("accounts/", include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
 ]
