08f3c38b904d442c0de4e7270bc31cd2a554bee9
Harish <quteharry19@hotmail.com>
2019-10-07 10:34:50 +0530

    check azure

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -2,7 +2,7 @@ import os
 from waitress import serve  
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosql.settings")  
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizza.settings")  
 
 application = get_wsgi_application()  
 
